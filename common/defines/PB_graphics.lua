2b8cdcf8f816ce5dde072a0ac853d8aad53152dd
GOD16 <61933587+GOD16@users.noreply.github.com>
2021-10-21 08:04:50 +1030

    Mud Fix (I think)

---
+++ common/defines/PB_graphics.lua
@@ -0,0 +1,7 @@
+-- Fuck the mud texture
+
+NDefines.NGraphics.SUN_HEIGHT = 1200
+NDefines.NGraphics.SUN_INTENSITY = 0.9
+NDefines_Graphics.NGraphics.BLOOM_SCALE = 0.0
+NDefines.NGraphics.COUNTRY_COLOR_SATURATION_MODIFIER = 0.9
+NDefines.NGraphics.GRADIENT_BORDERS_CAMERA_DISTANCE_OVERRIDE_COUNTRY = 0.08
