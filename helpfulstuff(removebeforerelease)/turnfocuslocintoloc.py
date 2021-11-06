8ea2ed5b58ccc40637069cddd62bfcc879fef843
Arj <33406133+ArjtheGreat@users.noreply.github.com>
2021-11-06 08:53:00 -0700

    germany tree rework

--- helpfulstuff(removebeforerelease)/turnfocuslocintoloc.py
+++ helpfulstuff(removebeforerelease)/turnfocuslocintoloc.py
@@ -3,10 +3,10 @@
 import sys
 #myFile = open(r"C:\Users\17138\Documents\Paradox Interactive\Hearts of Iron IV\mod\dbkgithub\common\national_focus\ITA_socialist_paths.txt") ##file path
 
-myFile = open(r"C:\Users\17138\Documents\Paradox Interactive\Hearts of Iron IV\mod\PaxBritannicaGithub\common\national_focus\PB_Russia_Alexandra_Tree.txt") ##file path
+myFile = open(r"C:\Users\Arj\Documents\Paradox Interactive\Hearts of Iron IV\mod\PaxBritannicaGithub\common\national_focus\PB_Germany.txt") ##file path
 substring = "id ="
 substring2 = "relative"
-substring3 = "PB_RUS_" ##put your country tag here
+substring3 = "PB_GER_" ##put your country tag here
 
 for line in myFile:
     if(substring in line):
