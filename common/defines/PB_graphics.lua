d37caf8fd890edf044e2ff9ead02f084ad74b230
LeXus <polski.gracz.pl1@gmail.com>
2024-10-05 08:42:32 +0200

    grind never stops

--- common/defines/PB_graphics.lua
+++ common/defines/PB_graphics.lua
@@ -7,6 +7,8 @@ NDefines.NGraphics.COUNTRY_COLOR_SATURATION_MODIFIER = 0.9
 NDefines.NGraphics.GRADIENT_BORDERS_CAMERA_DISTANCE_OVERRIDE_COUNTRY = 0.08
 NDefines.NGraphics.TREE_FADE_NEAR = 150.0
 NDefines.NGraphics.TREE_FADE_FAR = 300.0
+NDefines_Graphics.NFrontend.CAMERA_MIN_HEIGHT = 25.0
+NDefines_Graphics.NFrontend.CAMERA_MAX_HEIGHT = 3500.0
 
 -- useful for high camera state name display, uncomment when needed
 -- NDefines.NGraphics.PROVINCE_NAME_DRAW_DISTANCE = 2000.0
