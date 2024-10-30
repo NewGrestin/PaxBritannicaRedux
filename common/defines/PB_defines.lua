b9e04819f574234b9b8c2c288ab121203922e3c7
LeXus <polski.gracz.pl1@gmail.com>
2024-10-30 21:06:45 +0100

    more

--- common/defines/PB_defines.lua
+++ common/defines/PB_defines.lua
@@ -1,3 +1,5 @@
+NDefines.NGame.START_DATE = "1933.1.1.11"
+NDefines.NGame.END_DATE = "2000.1.1.1"
 NDefines.NFocus.FOCUS_POINT_DAYS = 1
 NDefines.NCountry.MIN_STABILITY = -1.0
 NDefines.NCountry.MIN_WAR_SUPPORT = -1.0
