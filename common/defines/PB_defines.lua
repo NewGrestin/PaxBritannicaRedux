81ed8e0500e2bf836f3d82722724806da9250053
LeXus <polski.gracz.pl1@gmail.com>
2025-01-30 21:50:01 +0100

    small additions

--- common/defines/PB_defines.lua
+++ common/defines/PB_defines.lua
@@ -8,3 +8,17 @@ NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 750
 NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 750
 NDefines.NMilitary.MAX_AIR_EXPERIENCE = 750
 NDefines.NMilitary.MAX_DIVISION_SUPPORT_WIDTH = 2;
+
+NDefines.NMilitary.UNIT_LEADER_INITIAL_TRAIT_SLOT = { 				-- trait slot for 0 level leader
+2.0, -- field marshal  vanilla 1
+1.0, -- corps commander  vanilla 0
+1.0, -- navy general
+0.0, -- operative
+}
+
+NDefines.NMilitary.UNIT_LEADER_TRAIT_SLOT_PER_LEVEL = { 			-- num extra traits on each level
+0.75, -- field marshal  vanilla 0.5
+0.75, -- corps commander  vanilla 0.5
+0.75, -- navy general  vanilla 0.5
+0.0, -- operative
+}
