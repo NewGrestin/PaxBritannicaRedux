6040b4eda0801ca682efaa91b6b59581569c9b75
LeXus <polski.gracz.pl1@gmail.com>
2025-12-19 13:45:50 +0100

    more stuff

--- common/defines/PB_defines.lua
+++ common/defines/PB_defines.lua
@@ -1,15 +1,30 @@
+-- Game
 NDefines.NGame.START_DATE = "1933.1.1.11"
 NDefines.NGame.END_DATE = "2000.1.1.1"
+NDefines.NGame.HANDS_OFF_START_TAG = "MTA" -- Mount Athos is and will not be involved in anything yet
+NDefines.NCountry.BASE_MAX_COMMAND_POWER = 150.0
+-- Focus
 NDefines.NFocus.FOCUS_POINT_DAYS = 1
+-- Diplomacy
+NDefines.NDiplomacy.MAX_TRUST_VALUE = 200									-- Vanilla is 100
+NDefines.NDiplomacy.MIN_TRUST_VALUE = -200									-- Vanilla is -100
+NDefines.NDiplomacy.MAX_OPINION_VALUE = 200									-- Vanilla is 100
+NDefines.NDiplomacy.MIN_OPINION_VALUE = -200								-- Vanilla is -100
+NDefines.NDiplomacy.BASE_TRUCE_PERIOD = 400									-- Vanilla is 180
+NDefines.NDiplomacy.VERY_GOOD_OPINION = 100									-- Vanilla is 50
+NDefines.NDiplomacy.VERY_BAD_OPINION = -100									-- Vanilla is -50
+NDefines.NDiplomacy.FRONT_IS_DANGEROUS = 0									-- Vanilla is -100
+NDefines.NAI.DIPLOMACY_ACCEPT_CONDITIONAL_SURRENDER_GLOBAL_TENSION = -1000	-- Vanilla is -10. Adds -10 to conditional surrender acceptance per 1 WT point - disables conditional surrender for AI
+-- Country
 NDefines.NCountry.MIN_STABILITY = -1.0
 NDefines.NCountry.MIN_WAR_SUPPORT = -1.0
-NDefines.NCountry.BASE_MAX_COMMAND_POWER = 150.0
+-- Military
 NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 750
 NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 750
 NDefines.NMilitary.MAX_AIR_EXPERIENCE = 750
 NDefines.NMilitary.MAX_DIVISION_SUPPORT_WIDTH = 2
-
 NDefines.NMilitary.SLOWEST_SPEED = 2 -- yes i had to change this
+-- Buildings
 NDefines.NBuildings.MAX_SHARED_SLOTS = 30
 
 -- Career Profile
